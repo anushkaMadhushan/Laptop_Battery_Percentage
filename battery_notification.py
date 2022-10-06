603a8180e6ac89477823a330e3ce88eb7231266a
Anushka <anushkamadhushankulathunga@gmail.com>
2022-10-06 08:48:49 +0530

    Completed

---
+++ battery_notification.py
@@ -0,0 +1,15 @@
+
+from email import message
+import time
+import psutil
+from plyer import notification
+
+
+battery = psutil.sensors_battery()
+percent = battery.percent
+
+notification.notify(
+    title = "Battery Percentage",
+    message = str(percent)+"% Battery Remaining",
+    timeout = 20
+)
